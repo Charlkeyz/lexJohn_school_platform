7ce961b5da71215c97cf9398d2353de14ef72b8b
unknown <beejayolawumi2020@gmail.com>
2024-05-09 21:28:17 +0100

    mrge

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import './App.css'
-import HomePage from './LexJohn Pages/Home Page/HomePage'
+import "./App.css";
+import HomePage from "./LexJohn Pages/Home Page/HomePage";
 import { HashRouter, Routes, Route } from "react-router-dom";
-
+import AboutUs from "./components/aboutPageComponent/aboutus/AboutUs";
+import ContactPage from "./components/contactPage/ContactPage";
 
 function App() {
-  
-
   return (
     <>
       <HashRouter hashType="hashbang">
         <Routes>
-          <Route path='/' element={<HomePage/>}/>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/contact" element={<ContactPage />} />
         </Routes>
       </HashRouter>
-      
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
