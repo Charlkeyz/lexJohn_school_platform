c491e0a6ea594e7fbc70c4da3e54ac38f1f4fa0f
unknown <beejayolawumi2020@gmail.com>
2024-05-09 12:39:48 +0100

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import './App.css'
-import HomePage from './LexJohn Pages/Home Page/HomePage'
+import "./App.css";
+import HomePage from "./LexJohn Pages/Home Page/HomePage";
 import { HashRouter, Routes, Route } from "react-router-dom";
-
 
 function App() {
   return (
     <>
       <HashRouter hashType="hashbang">
         <Routes>
-          <Route path='/' element={<HomePage/>}/>
+          <Route path="/" element={<HomePage />} />
         </Routes>
       </HashRouter>
-      
-      
     </>
   );
 }
