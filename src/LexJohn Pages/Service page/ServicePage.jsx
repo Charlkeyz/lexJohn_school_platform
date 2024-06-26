2a3588c8f334dfe4d8eac07769200fe08ee0b4a2
Charles ugochukwu <Charlkeyz2017@gmail.com>
2024-06-26 21:23:43 +0100

    few changes made

--- src/LexJohn Pages/Service page/ServicePage.jsx
+++ src/LexJohn Pages/Service page/ServicePage.jsx
@@ -17,9 +17,13 @@ const ServicePage = () => {
     AOS.init({
       duration: 1000, // Animation duration in milliseconds
       once: true,
-      offset: 100,     // Whether animation should happen only once - while scrolling down
+          
     });
   }, []);
+
+  const viewportHeight = window.innerHeight;
+  const middleOfViewport = viewportHeight / 2;
+  
 
 
   return (
@@ -28,19 +32,19 @@ const ServicePage = () => {
         <section className="flex flex-col justify-center items-center w-screen">
           <div className="flex flex-col sm:flex-row sm:w-[1065px]  w-screen h-fit sm:h-fit justify-center items-center mt-20">
             <div className="flex flex-col justify-center items-center sm:items-start sm:gap-10 gap-5 p-2">
-              <h1 className="text-[40px] font-bold leading-[48px]" data-aos="fade-down">What we do</h1>
+              <h1 className="text-[40px] font-bold leading-[48px]" data-aos="fade-down" data-aos-offset={middleOfViewport}>What we do</h1>
               <p className="sm:text-base text-sm text-center sm:text-start font-normal leading-6 text-text-color"data-aos="fade-up">Lorem ipsum dolor sit amet consectetur adipisicing elit.
                  Dolor libero ea voluptatem ad dolore, aut quibusdam cumque voluptas necessitatibus autem distinctio aliquid dolorem exercitationem itaque repellat? Natus, rerum! Explicabo, recusandae?
               </p>
             </div>
-            <img data-aos="fade-left" data-aos-delay="200" data-aos-duration="1000" src={ServiceImg} alt="" />
+            <img data-aos="fade-left" data-aos-delay="200" data-aos-duration="1000" data-aos-offset={middleOfViewport} src={ServiceImg} alt="" />
           </div>
         
             <Companies className='bg-primary text-white w-screen' text_white='text-white'/>
             <div className="flex flex-col justify-center gap-10 mt-10">
               <div className="flex flex-col justify-center items-center">
                 <h1 className="text-2xl font-semibold leading-10">Our Expertise</h1>
-                <p data-aos="fade-left" data-aos-delay="100" className="sm:text-base text-sm text-text-color p-2 sm:p-0 font-normal leading-6 sm:w-[1115px] w-screen text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <p data-aos="fade-left" data-aos-delay="100" data-aos-offset={middleOfViewport} className="sm:text-base text-sm text-text-color p-2 sm:p-0 font-normal leading-6 sm:w-[1115px] w-screen text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit.
                    Voluptates quidem corporis ut maiores nobis dolorem cupiditate aut animi illum,
                     quos iure distinctio delectus, perspiciatis praesentium facilis pariatur laborum quo esse.
                 </p>
